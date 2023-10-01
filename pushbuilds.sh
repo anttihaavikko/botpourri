68239e0ff3dd3dfca3e3efe8ee3ffaf67a82f1f9
Antti Haavikko <antti.haavikko@gmail.com>
2023-10-01 20:09:14 +0300

    Update build pushing script

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,15 +4,16 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/replace:win --fix-permissions
+butler push Builds/win anttihaavikko/botpourris-space-chase:win --fix-permissions
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/replace:osx --fix-permissions
+butler push Builds/osx anttihaavikko/botpourris-space-chase:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/replace:linux
+butler push Builds/linux anttihaavikko/botpourris-space-chase:linux
 
-echo "${COLOR}Copying html5 files over to correct path"
-cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+# echo "${COLOR}Copying html5 files over to correct path"
+# cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+
 echo "${COLOR}Pushing build for HTML5${NC}"
-butler push Builds/html5 anttihaavikko/replace:html5
+butler push Builds/html5 anttihaavikko/botpourris-space-chase:html5
